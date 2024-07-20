5afac96514bf789c476a04c1de26ed329aac2953
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-07-20 00:43:34 -0300

    feat: translate for portuguese

--- src/main/java/com/desafiobackendmagazord/exceptions/RecipeNotFoundException.java
+++ src/main/java/com/desafiobackendmagazord/exceptions/RecipeNotFoundException.java
@@ -2,6 +2,6 @@ package com.desafiobackendmagazord.exceptions;
 
 public class RecipeNotFoundException  extends RuntimeException{
     public RecipeNotFoundException(String id) {
-        super("Recipe not found with id: " + id);
+        super("Receita não encontrada: " + id);
     }
 }
