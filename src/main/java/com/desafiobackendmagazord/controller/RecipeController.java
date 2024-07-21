0a3be07d0921b55c65f37ac63a0d24f06d9e4634
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-07-21 14:16:48 -0300

    feat: add endpoints showRecipeBySearch, saveRecipeComment, updateRecipeComments, and deleteCommentsById in RecipeController

--- src/main/java/com/desafiobackendmagazord/controller/RecipeController.java
+++ src/main/java/com/desafiobackendmagazord/controller/RecipeController.java
@@ -1,9 +1,11 @@
 package com.desafiobackendmagazord.controller;
 
+import com.desafiobackendmagazord.domain.Comments;
 import com.desafiobackendmagazord.domain.Recipe;
 import com.desafiobackendmagazord.dto.RecipeDTO;
 import com.desafiobackendmagazord.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -52,6 +54,36 @@ public class RecipeController {
     @GetMapping("/ingredient")
     public List<Recipe> showRecipeByIngredient(@RequestParam String ingredients){
         return recipeService.showRecipeByIngredient(ingredients);
+    }
+
+    @GetMapping("/search")
+    public List<Recipe> showRecipeBySearch(@RequestParam String search ){
+        return recipeService.showRecipeBySearch(search);
+    }
+
+    @PostMapping("/{id}/comment")
+    public Comments saveRecipeComments(@PathVariable("id") String id, @RequestBody Comments comments){
+       return  recipeService.saveRecipeComments(id,comments);
+    }
+
+    @PutMapping("/{id}/comment/{commentId}")
+    public ResponseEntity<String> updateRecipeComments(@PathVariable String id, @PathVariable String commentId, @RequestBody Comments comments) {
+        try {
+            boolean updated = recipeService.updateRecipeComments(id, commentId, comments);
+            if (updated) {
+                return ResponseEntity.ok("Comentário atualizado com sucesso");
+            } else {
+                System.out.println("Comentário não encontrado para a receita com Id : " + id + " e comentário com ID: " + commentId);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Comentário não encontrado");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error ao atualizar o comentário: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}/comment/{commentsId}")
+        public ResponseEntity<String> deleteRecipeComments(@PathVariable String id, @PathVariable String commentsId){
+        return  recipeService.deleteCommentsById(id,commentsId);
     }
 
 }
