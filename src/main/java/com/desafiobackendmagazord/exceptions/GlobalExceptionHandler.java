31f481cb82fbe3997ea114c62640b86437a3e933
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-07-21 14:10:50 -0300

    feat: create exceptions

--- src/main/java/com/desafiobackendmagazord/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/desafiobackendmagazord/exceptions/GlobalExceptionHandler.java
@@ -11,6 +11,29 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RecipeNotFoundException.class)
     public ResponseEntity<String> handleRecipeNotFoundException(RecipeNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+
+    }
+
+    @ExceptionHandler(SearchNotFoundException.class)
+    public ResponseEntity<String> handleSearchNotFoundException(SearchNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+
+    }
+
+    @ExceptionHandler(CommentsException.class)
+    public ResponseEntity<String> handleCommentsException(CommentsException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+
+    }
+    @ExceptionHandler(CommentsIdNotFound.class)
+    public ResponseEntity<String> handleCommentsIdException(CommentsIdNotFound ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+
+    }
+    @ExceptionHandler(DeleteCommentsMessage.class)
+    public ResponseEntity<String> handleCommentsDelete(DeleteCommentsMessage ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+
     }
 
 }
