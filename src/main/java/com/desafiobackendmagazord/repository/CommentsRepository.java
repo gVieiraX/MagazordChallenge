cf495d5dbf8cdb50eecf0fcf92c2648b9279840a
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-07-21 14:11:50 -0300

    feat: create CommentsRepository and add annotation @Repository in RecipeRepository

--- src/main/java/com/desafiobackendmagazord/repository/CommentsRepository.java
+++ src/main/java/com/desafiobackendmagazord/repository/CommentsRepository.java
@@ -1,10 +1,9 @@
 package com.desafiobackendmagazord.repository;
 
-import com.desafiobackendmagazord.domain.Recipe;
+import com.desafiobackendmagazord.domain.Comments;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface RecipeRepository extends MongoRepository<Recipe,String> {
+public interface CommentsRepository extends MongoRepository<Comments,String> {
 }
