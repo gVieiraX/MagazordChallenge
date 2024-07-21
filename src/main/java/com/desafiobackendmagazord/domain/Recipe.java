25b83a2b574c230b92b27b5f25113235b71faf38
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-07-21 14:19:18 -0300

    feat: add List<Comments> comments attribute to Recipe class and RecipeDTO

--- src/main/java/com/desafiobackendmagazord/domain/Recipe.java
+++ src/main/java/com/desafiobackendmagazord/domain/Recipe.java
@@ -1,11 +1,14 @@
 package com.desafiobackendmagazord.domain;
 
 import com.desafiobackendmagazord.dto.RecipeDTO;
+import com.desafiobackendmagazord.exceptions.CommentsException;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -18,20 +21,22 @@ public class Recipe  {
     private String description;
     private List<String> likes;
     private List<String> ingredients;
+    private List<Comments> comments;
 
-    public Recipe(String title, String description, List<String> likes, List<String> ingredients) {
+    public Recipe(String title, String description, List<String> likes, List<String> ingredients, List<Comments> comments) {
         this.title = title;
         this.description = description;
         this.likes = likes;
         this.ingredients = ingredients;
+        this.comments=comments;
     }
-
 
     public Recipe(RecipeDTO recipeDTO) {
         this.title = recipeDTO.getTitle();
         this.description = recipeDTO.getDescription();
         this.likes = recipeDTO.getLikes();
         this.ingredients = recipeDTO.getIngredients();
+        this.comments = recipeDTO.getComments();
     }
 
     public Recipe() {
@@ -39,6 +44,5 @@ public class Recipe  {
 
     public Recipe(String title, String description, List<String> ingredients) {
     }
-
 
 }
--- src/main/java/com/desafiobackendmagazord/dto/RecipeDTO.java
+++ src/main/java/com/desafiobackendmagazord/dto/RecipeDTO.java
@@ -1,12 +1,10 @@
 package com.desafiobackendmagazord.dto;
 
-import com.desafiobackendmagazord.domain.Recipe;
+import com.desafiobackendmagazord.domain.Comments;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
-
-import static com.sun.beans.introspect.PropertyInfo.Name.description;
 
 @Getter
 @Setter
@@ -15,16 +13,18 @@ public class RecipeDTO {
     private String description;
     private List<String> likes;
     private List<String> ingredients;
+    private List<Comments> comments;
 
     // Construtor padrão
     public RecipeDTO() {}
 
     // Construtor completo
-    public RecipeDTO(String title, String description, List<String> likes, List<String> ingredients) {
+    public RecipeDTO(String title, String description, List<String> likes, List<String> ingredients,List<Comments> comments) {
         this.title = title;
         this.description = description;
         this.likes = likes;
         this.ingredients = ingredients;
+        this.comments = comments;
     }
 
 }
